4beca588150d4404944d12e83e220cafa8ab875c
NarahariRohan <rohan.r@labournet.in>
2020-05-20 14:59:38 +0530

    enrolment bulk upload - issue

--- Database/db.py
+++ Database/db.py
@@ -4720,86 +4720,51 @@ SELECT					cb.name as candidate_name,
         cur.close()
         con.close()
         return response
-    def enrollment_web_inser(df,user_id):
+    def mobilization_web_inser(df,user_id):
+        con = pyodbc.connect(conn_str)
+        cur = con.cursor()
         try:
-            conn = pyodbc.connect(conn_str)
-            curs = conn.cursor()
-
-            df['Date of Birth*'] = df['Date of Birth*'].astype(str)
-            out = df.values.tolist()
             quer1 = '''
-            update candidate_details.tbl_candidates set isFresher={},isDob={},years_of_experience='{}',salutation='{}',first_name='{}',middle_name='{}',last_name='{}',date_of_birth='{}',age='{}',primary_contact_no='{}',secondary_contact_no='{}',email_id='{}',gender='{}',marital_status='{}',caste='{}',disability_status='{}',religion='{}',source_of_information='{}', present_district='{}', present_state=(select state_id from masters.tbl_states where state_name like trim('{}')),present_pincode='{}',present_country=(select country_id from masters.tbl_countries where country_name like trim('{}')),permanent_district='{}',permanent_state=(select state_id from masters.tbl_states where state_name like trim('{}')),permanent_pincode='{}',permanent_country=(select country_id from masters.tbl_countries where country_name like trim('{}')), candidate_stage_id=3,candidate_status_id=2,created_on=GETDATE(),created_by='{}',is_active=1 where candidate_id='{}';
+            insert into candidate_details.tbl_candidates
+            (isFresher, salutation, first_name, middle_name, last_name, date_of_birth, isDob, age,primary_contact_no, secondary_contact_no, email_id, gender,marital_status, caste, disability_status, religion, source_of_information, present_pincode,present_district, permanent_district,permanent_pincode,candidate_stage_id, candidate_status_id, created_on, created_by, is_active, insert_from,permanent_state,permanent_country,present_state, present_country)
+            OUTPUT inserted.candidate_id
+            values
             '''
             quer2='''
-            update candidate_details.tbl_candidate_reg_enroll_details set candidate_photo='{}',mother_tongue='{}',current_occupation='{}',average_annual_income='{}',interested_course='{}',product='{}',present_address_line1='{}',permanaet_address_line1='{}',aadhar_no='{}',identifier_type=(select identification_id from masters.tbl_identification_type where UPPER(identification_name)=UPPER('{}')),identity_number='{}',document_copy_image_name='{}',employment_type='{}',preferred_job_role='{}',relevant_years_of_experience='{}',current_last_ctc='{}',preferred_location='{}',willing_to_travel='{}',willing_to_work_in_shifts='{}',bocw_registration_id='{}',expected_ctc='{}',highest_qualification='{}',stream_specialization='{}',computer_knowledge='{}',technical_knowledge='{}',family_salutation='{}',member_name='{}',gender='{}',education_qualification='{}',relationship='{}',occupation='{}',average_household_income='{}',bank_name='{}',account_number='{}',created_by='{}',created_on=GETDATE(),is_active=1 where candidate_id='{}';
+            insert into candidate_details.tbl_candidate_reg_enroll_details
+            (candidate_id,candidate_photo,present_address_line1,permanaet_address_line1,created_on,created_by,is_active)
+            values
             '''
             quer3='''
-            update candidate_details.tbl_candidate_reg_enroll_non_mandatory_details set present_address_line2='{}',present_village='{}',present_panchayat='{}',present_taluk_block='{}',permanent_address_line2='{}',permanent_village='{}',permanent_panchayat='{}',permanent_taluk_block='{}',name_of_institute='{}',university='{}',year_of_pass='{}',percentage='{}',family_date_of_birth='{}',family_age='{}',family_primary_contact='{}',family_email_address='{}',branch_name='{}',branch_code='{}',account_type='{}',attachment_image_name='{}',created_by='{}',created_on=GETDATE(),is_active=1 where candidate_id='{}';
-            '''
-            quer4='''
-            insert into candidate_details.tbl_candidate_interventions
-            (candidate_id,intervention_category,created_on,created_by,is_active)
-            OUTPUT inserted.candidate_intervention_id
+            insert into candidate_details.tbl_candidate_reg_enroll_non_mandatory_details
+            (candidate_id,present_address_line2,present_village,present_panchayat,present_taluk_block,permanent_address_line2,permanent_village,permanent_panchayat,permanent_taluk_block,created_on,created_by,is_active)
             values
             '''
-            quer5='''
-            insert into candidate_details.tbl_map_candidate_intervention_skilling
-            (intervention_id, course_id, batch_id, intervention_value, created_on,created_by,is_active)
-            values
-            '''
-            quer6='''
-            update	candidate_details.tbl_map_candidate_intervention_skilling set batch_id=(select batch_id from batches.tbl_batches where batch_code=trim('{}')) where intervention_id='{}'
-            '''
-            query = ""
-            b=[]
-            temp=""
+            df['Date of Birth*'] = df['Date of Birth*'].astype(str)
+            out = df.values.tolist()
             for row in out:
-                #que = '''select candidate_intervention_id from candidate_details.tbl_candidate_interventions where candidate_id='{}' '''.format(row[0])
-                que='''
-                        SELECT		cs.intervention_id 
-                        FROM		candidate_details.tbl_candidate_interventions i
-                        LEFT JOIN	candidate_details.tbl_map_candidate_intervention_skilling cs on cs.intervention_id= i.candidate_intervention_id 
-                        WHERE		i.candidate_id={}
-                        AND			cs.intervention_id is NOT NULL
-                    '''.format(row[0])
-                curs.execute(que)
-                intervention_id = curs.fetchall()
-                if intervention_id!=[]:
-                    query += quer6.format(row[80],intervention_id[0][0])
-                else:
-                    b.append(row[80])
-                    temp += '\n' + "({},'SAE',GETDATE(),{},1),".format(row[0],user_id)
-                
-                query += '\n' + quer1.format(1 if str(row[1]).lower()=='Fresher' else 0, 1 if row[8]=='' else 0,row[47],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[20],row[28],row[29],row[30],row[31],row[37],row[38],row[39],row[40],user_id,row[0])
-                query += '\n' + quer2.format(row[2],row[17],row[18],row[19],row[21],row[22],row[23],row[32],row[41],row[42],row[43],row[44],row[45],row[46],row[48],row[49],row[50],row[51],row[52],row[53],row[54],row[55],row[56],row[61],row[62],row[63],row[64],row[69],row[70],row[71],row[72],row[73],row[74],row[78],user_id,row[0])
-                query += '\n' + quer3.format(row[24],row[25],row[26],row[27],row[33],row[34],row[35],row[36],row[57],row[58],row[59],row[60],row[65],row[66],row[67],row[68],row[75],row[76],row[77],row[79],user_id,row[0])
-            
-            curs.execute(query)
-            curs.commit()
-            d=[]
-            if temp!="":
-                #print(temp)  
-                quer4 =quer4 + temp[:-1]+';'                          
-                curs.execute(quer4)
-                d = list(map(lambda x:x[0],curs.fetchall()))
-                curs.commit()
-            
-            temp2=""
+                quer = "({},'{}','{}','{}','{}','{}',{},'{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}',1,2,GETDATE(),{},1,'w',{},'{}',{},'{}'),".format(1 if row[0]=='Fresher' else 0,row[2],row[3],row[4],row[5],row[6],
+                        1 if row[7]=='' else 0,row[7] if row[7]!='' else 0,row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[24],row[22],row[31],row[33],user_id,"(select state_id from masters.tbl_states where state_name like trim('{}'))".format(row[23]),
+                        '1',"(select state_id from masters.tbl_states where state_name like trim('{}'))".format(row[32]),'1')
+                quer1 += '\n'+quer
+            quer1 = quer1[:-1]+';'
+            cur.execute(quer1)
+            d = list(map(lambda x:x[0],cur.fetchall()))
+            cur.commit()
             for i in range(len(d)):
-                temp2 += '\n' + "({},(select course_id from batches.tbl_batches where batch_code=trim('{}')),(select batch_id from batches.tbl_batches where batch_code=trim('{}')),concat('ENR',(NEXT VALUE FOR candidate_details.sq_candidate_enrollment_no)),GETDATE(),{},1),".format(d[i],b[i],b[i],user_id)
-            if temp2!="":
-                quer5 = quer5+ temp2[:-1]+';'
-                #print(quer5)
-                curs.execute(quer5)
-                curs.commit()
-
+                quer2 += '\n' + "({},'{}','{}','{}',GETDATE(),{},1),".format(d[i],out[i][1],out[i][17],out[i][26],user_id)
+                quer3 += '\n' + "({},'{}','{}','{}','{}','{}','{}','{}','{}',GETDATE(),{},1),".format(d[i],out[i][18],out[i][19],out[i][20],out[i][21],out[i][27],out[i][28],out[i][29],out[i][30],user_id)
+            quer2 = quer2[:-1]+';'
+            quer3 = quer3[:-1]+';'
+            cur.execute(quer2 + '\n' + quer3)
+            cur.commit()
             out = {'Status': True, 'message': "Submitted Successfully"}
         except Exception as e:
             out = {'Status': False, 'message': "error: "+str(e)}
         finally:
-            curs.close()
-            conn.close()
-            return out
+            cur.close()
+            con.close()
+            return out    
     
     def AllCreatedByBasedOnUser(UserId,UserRoleId):
         response=[]
@@ -4901,15 +4866,23 @@ SELECT					cb.name as candidate_name,
             '''
             query = ""
             b=[]
+            temp=""
             for row in out:
-                que = '''select candidate_intervention_id from candidate_details.tbl_candidate_interventions where candidate_id='{}' '''.format(row[0])
+                #que = '''select candidate_intervention_id from candidate_details.tbl_candidate_interventions where candidate_id='{}' '''.format(row[0])
+                que='''
+                        SELECT		cs.intervention_id 
+                        FROM		candidate_details.tbl_candidate_interventions i
+                        LEFT JOIN	candidate_details.tbl_map_candidate_intervention_skilling cs on cs.intervention_id= i.candidate_intervention_id 
+                        WHERE		i.candidate_id={}
+                        AND			cs.intervention_id is NOT NULL
+                    '''.format(row[0])
                 curs.execute(que)
                 intervention_id = curs.fetchall()
                 if intervention_id!=[]:
                     query += quer6.format(row[80],intervention_id[0][0])
                 else:
                     b.append(row[80])
-                    quer4 += '\n' + "({},'SAE',GETDATE(),{},1),".format(row[0],user_id)
+                    temp += '\n' + "({},'SAE',GETDATE(),{},1),".format(row[0],user_id)
                 
                 query += '\n' + quer1.format(1 if str(row[1]).lower()=='Fresher' else 0, 1 if row[8]=='' else 0,row[47],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[20],row[28],row[29],row[30],row[31],row[37],row[38],row[39],row[40],user_id,row[0])
                 query += '\n' + quer2.format(row[2],row[17],row[18],row[19],row[21],row[22],row[23],row[32],row[41],row[42],row[43],row[44],row[45],row[46],row[48],row[49],row[50],row[51],row[52],row[53],row[54],row[55],row[56],row[61],row[62],row[63],row[64],row[69],row[70],row[71],row[72],row[73],row[74],row[78],user_id,row[0])
@@ -4917,17 +4890,22 @@ SELECT					cb.name as candidate_name,
             
             curs.execute(query)
             curs.commit()
+            d=[]
+            if temp!="":
+                #print(temp)  
+                quer4 =quer4 + temp[:-1]+';'                          
+                curs.execute(quer4)
+                d = list(map(lambda x:x[0],curs.fetchall()))
+                curs.commit()
             
-            quer4 = quer4[:-1]+';'
-            curs.execute(quer4)
-            d = list(map(lambda x:x[0],curs.fetchall()))
-            
-            curs.commit()
+            temp2=""
             for i in range(len(d)):
-                quer5 += '\n' + "((select batch_id from batches.tbl_batches where batch_code=trim('{}'),(select course_id from batches.tbl_batches where batch_code=trim('{}')),{},concat('ENR',(NEXT VALUE FOR candidate_details.sq_candidate_enrollment_no)),GETDATE(),{},1),".format(d[i],b[i],b[i],user_id)
-            quer5 = quer5[:-1]+';'
-            curs.execute(quer5)
-            curs.commit()
+                temp2 += '\n' + "({},(select course_id from batches.tbl_batches where batch_code=trim('{}')),(select batch_id from batches.tbl_batches where batch_code=trim('{}')),concat('ENR',(NEXT VALUE FOR candidate_details.sq_candidate_enrollment_no)),GETDATE(),{},1),".format(d[i],b[i],b[i],user_id)
+            if temp2!="":
+                quer5 = quer5+ temp2[:-1]+';'
+                #print(quer5)
+                curs.execute(quer5)
+                curs.commit()
 
             out = {'Status': True, 'message': "Submitted Successfully"}
         except Exception as e:
