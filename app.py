fac8bcedb744bc9ed9e5faf8bd21ddb04c80271c
NarahariRohan <rohan.r@labournet.in>
2020-08-03 12:33:39 +0530

    submit she details

--- app.py
+++ app.py
@@ -7293,5 +7293,20 @@ class updated_new_SL4Report(Resource):
                 return {"exceptione":str(e)}
 api.add_resource(updated_new_SL4Report,'/updated_new_SL4Report')
 
+
+@app.route("/candidate_data_page")
+def candidate_data_page():
+    if g.user:
+        return render_template("Reports/candidate-data.html")
+    else:
+        return render_template("login.html",error="Session Time Out!!")
+
+@app.route("/candidate_data")
+def candidate_data():
+    if g.user:
+        return render_template("home.html",values=g.User_detail_with_ids,html="candidate_data_page")
+    else:
+        return render_template("login.html",error="Session Time Out!!")
+
 if __name__ == '__main__':
     app.run(debug=True)
