b39ad063e0fc5993f121a47ba6a5a986feb668c5
Jagdish Khatri <jagdish.pk@flabournet.in>
2021-01-08 11:24:24 +0530

    NEO-1032

--- Database/db.py
+++ Database/db.py
@@ -7303,17 +7303,61 @@ SELECT					cb.name as candidate_name,
         if int(role_id)==11:
             sql = 'exec [reports].[sp_get_ops_productivity_report_data_coo] ?, ?, ?,?,?'
             sql1 = 'exec [reports].[sp_get_ops_productivity_report_data_coo_sub_project] ?, ?, ?,?,?'
-            #sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_coo_course] ?, ?, ?,?,?'
+            sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_coo_course] ?, ?, ?,?,?'
         if int(role_id)==14:
             sql = 'exec [reports].[sp_get_ops_productivity_report_data_territory_manager] ?, ?, ?,?,?'
             sql1 = 'exec [reports].[sp_get_ops_productivity_report_data_territory_manager_sub_project] ?, ?, ?,?,?'
-            #sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_territory_manager_course] ?, ?, ?,?,?'
+            sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_territory_manager_course] ?, ?, ?,?,?'
         if int(role_id)==5:
             sql = 'exec [reports].[sp_get_ops_productivity_report_data_center_manager] ?, ?, ?,?,?'
             sql1 = 'exec [reports].[sp_get_ops_productivity_report_data_center_manager_sub_project] ?, ?, ?,?,?'
+            sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_center_manager_course] ?, ?, ?,?,?'
+        values = (customer_ids, contract_ids, month,user_id,user_role_id)
+        
+        curs.execute(sql,(values))
+        sheet1_columns = [column[0].title() for column in curs.description]        
+        data = curs.fetchall()
+        sheet1 = list(map(lambda x:list(x), data))
+        
+        curs.execute(sql1,(values))
+        sheet2_columns = [column[0].title() for column in curs.description]        
+        data = curs.fetchall()
+        sheet2 = list(map(lambda x:list(x), data))
+
+        curs.execute(sql2,(values))
+        sheet3_columns = [column[0].title() for column in curs.description]        
+        data = curs.fetchall()
+        sheet3 = list(map(lambda x:list(x), data))
+        return {'sheet1':sheet1,'sheet2':sheet2,'sheet3':sheet3,'sheet1_columns':sheet1_columns,'sheet2_columns':sheet2_columns,'sheet3_columns':sheet3_columns}
+        cur2.close()
+        con.close()
+
+    def DownloadEmployeeWiseReport(customer_ids,contract_ids,month,role_id,user_id,user_role_id):
+        con = pyodbc.connect(conn_str)
+        curs = con.cursor()
+        sheet1=[]
+        sheet1_columns=[]
+        sheet2=[]
+        sheet2_columns=[]
+        sheet3=[]
+        sheet3_columns=[]
+        sql=''
+        sql1=''
+        sql2=''
+        if int(role_id)==11:
+            sql = 'exec [reports].[sp_get_EmployeeWise_report_data_coo] ?, ?, ?,?,?'
+            sql1 = 'exec [reports].[sp_get_EmployeeWise_report_data_coo_sub_project] ?, ?, ?,?,?'
+            #sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_coo_course] ?, ?, ?,?,?'
+        if int(role_id)==14:
+            sql = 'exec [reports].[sp_get_EmployeeWise_report_data_territory_manager] ?, ?, ?,?,?'
+            sql1 = 'exec [reports].[sp_get_EmployeeWise_report_data_territory_manager_sub_project] ?, ?, ?,?,?'
+            #sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_territory_manager_course] ?, ?, ?,?,?'
+        if int(role_id)==5:
+            sql = 'exec [reports].[sp_get_EmployeeWise_report_data_center_manager] ?, ?, ?,?,?'
+            sql1 = 'exec [reports].[sp_get_EmployeeWise_report_data_center_manager_sub_project] ?, ?, ?,?,?'
             #sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_center_manager_course] ?, ?, ?,?,?'
         if int(role_id)==2:
-            sql = 'exec [reports].[sp_get_ops_productivity_report_data_mobilizer] ?, ?, ?,?,?'
+            sql = 'exec [reports].[sp_get_EmployeeWise_report_data_mobilizer] ?, ?, ?,?,?'
             #sql1 = 'exec [reports].[sp_get_ops_productivity_report_data_center_manager_sub_project] ?, ?, ?,?,?'
             #sql2 = 'exec [reports].[sp_get_ops_productivity_report_data_center_manager_course] ?, ?, ?,?,?'
         values = (customer_ids, contract_ids, month,user_id,user_role_id)
@@ -7343,7 +7387,7 @@ SELECT					cb.name as candidate_name,
         curs.close()
         con.close()
         return  res
-
+    
     def DownloadClientReport(user_id, user_role_id, client_id, funding_sources, customer_groups, category_type_ids):
         con = pyodbc.connect(conn_str)
         curs = con.cursor()
